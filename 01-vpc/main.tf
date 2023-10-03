d87a259ee640767ddc8149276d956fada29b7326
sivakumarreddy <info@joindevops.com>
2023-10-03 18:33:10 +0530

    SG rules

--- 01-vpc/main.tf
+++ 01-vpc/main.tf
@@ -1,5 +1,5 @@
 module "vpc" {
-  source = "../../terraform-aws-vpc-advanced"
+  source = "git::https://github.com/sivadevopsdaws74s/terraform-aws-vpc-advanced.git"
   project_name = var.project_name #"roboshop" instead of hard coding we are keeping in variables.tf
   cidr_block = var.cidr_block
   common_tags = var.common_tags
